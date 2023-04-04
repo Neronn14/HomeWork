cdfcfeea41b6af168174179330c1fb7e93c91b0b
Neronn14 <127049418+Neronn14@users.noreply.github.com>
2023-04-04 17:12:26 +0400

    Add files via upload

---
+++ scriipt.js
@@ -0,0 +1,19 @@
+// ! Task1 
+
+let a = 5;
+a = 6;
+let b = 6;
+b = 5;
+
+console.log(a,b);
+
+// ! Task2
+
+let emailLogin = 'ralisahib@bk.ru';
+let passwordLogin = 'Rasulov1997'
+
+let email = prompt ("enter your email");
+let password = prompt ("enter your password");
+
+console.log(emailLogin == email && passwordLogin == password  ? 'You have successfully logged in' : 'password or name is incorrect');
+
